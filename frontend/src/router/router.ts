623014dec5caa1184f14f99981876133f83739d8
vKnie <kevin.boillon@free.fr>
2025-03-20 16:53:41 +0100

    update router.ts

--- frontend/src/router/router.ts
+++ frontend/src/router/router.ts
@@ -8,13 +8,21 @@ import EspaceEtudiant from '../views/EspaceEtudiant.vue';
 import EspaceEnseignant from '../views/EspaceEnseignant.vue';
 import EspaceEntreprise from '../views/EspaceEntreprise.vue';
 
+import dashbord from '../views/dashbord.vue';
+
+import FormationsManager from '../views/admin/FormationsManager.vue';
+import UsersManager from '../views/admin/UserManager.vue';
+
 // Définition des routes
 const routes: Array<RouteRecordRaw> = [
   { path: '/departementGEII', component: DepartementGEII },
   { path: '/formation/:name', component: Formation, props: true },
   { path: '/espace-etudiant', component: EspaceEtudiant },
   { path: '/espace-enseignant', component: EspaceEnseignant },
-  { path: '/espace-entreprise', component: EspaceEntreprise }
+  { path: '/espace-entreprise', component: EspaceEntreprise },
+  { path : '/dashbord', component: dashbord },
+  { path : '/admin/formations', component: FormationsManager },
+  { path : '/admin/users', component: UsersManager }
 ];
 
 // Création du routeur
