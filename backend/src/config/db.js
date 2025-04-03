ca1002a655c53dfcec1caca2a8d446a876c523c9
vKnie <kevin.boillon@free.fr>
2025-04-03 12:19:51 +0200

    mini update

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -10,8 +10,13 @@ const db = mysql.createConnection({
 });
 
 db.connect((err) => {
+  console.log('DB_HOST:', process.env.DB_HOST);
+  console.log('DB_PORT:', process.env.DB_PORT);
+  console.log('DB_USER:', process.env.DB_USER);
+  console.log('DB_NAME:', process.env.DB_NAME);
   if (err) {
-    console.error("❌ Erreur de connexion à MariaDB:", err);
+    console.error("❌ Erreur de connexion à MariaDB:", err.code);
+    console.error("Détails de l'erreur:", err);
     return;
   }
   console.log("✅ Connecté à MariaDB !");
