80e5245f76d8790e3703ba793a1ed9da3fb19462
ZKSOnDaTrack <Kelyfane@gmail.com>
2025-04-03 15:21:17 +0200

    footer modif

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,10 @@
 <template>
-  <div>
-    <Navbar v-if="!isAdminRoute" />
-    <main>
-      <!-- Ici, tu peux mettre le contenu principal de tes pages -->
+  <div class="flex flex-col min-h-screen"> <!-- Utilisation de flex pour toute la page -->
+    <Navbar />
+    <main class="container mx-auto p-4 flex-grow"> <!-- Le contenu principal prend tout l'espace restant -->
       <router-view />
     </main>
-    <Footer v-if="!isAdminRoute" />
+    <Footer class="mt-auto" /> <!-- Le footer est toujours poussé vers le bas -->
   </div>
 </template>
 
@@ -18,11 +17,6 @@ export default {
   components: {
     Navbar,
     Footer
-  },
-  computed: {
-    isAdminRoute() {
-      return this.$route.path.startsWith('/admin');
-    }
   }
 }
 </script>
